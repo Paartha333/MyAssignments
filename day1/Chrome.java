52912f109fa82364eab3126984a2f0f92b9f9c6a
Paartha333 <paarthasarathy1999@gmail.com>
2025-03-02 13:52:02 +0530

    Add files via upload
    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Chrome {
+public static void main(String[] args) {
+	float version = 91.0f;
+	String developer ="Google";
+	boolean isbeta = false;
+	int releaseYear = 2008;
+	char shortcutKey = 'C';
+	System.out.println("Developer name :" +developer);
+	System.out.println("Version number :" +version);
+	System.out.println("Is it betatype :" +isbeta);
+	System.out.println("Year of release :" +releaseYear);
+	System.out.println("Shortcutkey is :" +shortcutKey);
+	
+}
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Firefox {
+	public static void main(String[] args) {
+		float browserVersion = 100.2f;
+		String browserName = "Firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		
+		System.out.println("Name of the browser is :" + browserName);
+		System.out.println("Version of the browser is :" + browserVersion);
+		System.out.println("Is browser visible :" + isVisible);
+		System.out.println("Year of release is :" + releaseYear);
+		System.out.println("The Logo of the browser is :" + browserLogo);
+	}
+
+}
